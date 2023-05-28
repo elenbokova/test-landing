4d09277c6363fd3b566d010f3974dfee73bac827
elenbokova <nansydrew@mail.ru>
2023-05-28 13:44:44 +0400

    Add files

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
+import {Burger} from './modules/burger';
 
 // ---------------------------------
 
@@ -17,10 +16,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
-    const form = new Form();
-    window.form = form;
-    form.init();
+    const burger = new Burger();
+    burger.init();
   });
 });
 
